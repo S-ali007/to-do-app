71747bed3c5d3e16e77e91ca22cb12160418d58f
ALI <alihak3er@daddy.com>
2023-03-18 19:07:59 +0500

    added two new features edit & update and description is later

--- file .js
+++ file .js
@@ -7,20 +7,31 @@ item.addEventListener(
     function (event) {
         if (event.key == "Enter") {
             addToDo(this.value);
-            // console.log(this.value);
-            this.value = "";
+            console.log(event);
+            this.value = " ";
         }
+        // else if (event.key == '     '){
+        //     document.getElementById('error').innerHTML="Please enter value";
+        //     addToDo(this.value);
+        //     this.value = "please enter something  ";
+        // }
+        // else   {
+        //     document.getElementById('error').innerHTML="Please enter value";
+        // }
     }
 )
-
 
 const addToDo = (item) => {
     const listItem = document.createElement("li");
     listItem.innerHTML = `
     ${item}   
       <i class="fa-regular fa-circle-xmark"></i>
+      <button id="edit" style="width: 20px;  ">
+      <img src="./assests/pen-to-square-solid.svg" alt="">
+    </button>
+    <button id="update" style="width: 20px ;">
+      <img src="./assests/pen-solid.svg" alt="">
     `;
-     
 
     listItem.addEventListener(
             "click" , function(){
