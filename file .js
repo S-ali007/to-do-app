cc6d03e87c70ca4298f0a7fa0c37e51bdddb1ffd
ALI <alihak3er@daddy.com>
2023-03-18 16:13:22 +0500

    user can remove the list item and changes about the line throug text

--- file .js
+++ file .js
@@ -1,22 +1,40 @@
-const item=document.querySelector("#item");
-const toDOBox=document.querySelector("#to-do-box");
+const item = document.querySelector("#item");
+const toDOBox = document.querySelector("#to-do-box");
 
 
 item.addEventListener(
     "keyup",
-    function(event){
-        if(event.key =="Enter"){
+    function (event) {
+        if (event.key == "Enter") {
             addToDo(this.value);
             // console.log(this.value);
-            this.value="";
+            this.value = "";
         }
     }
 )
 
 
-const addToDo =(item) => {
-    const listItem=document.createElement("li");
-    listItem.innerHTML=`${item}`;
+const addToDo = (item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+    ${item}   
+      <i class="fa-regular fa-circle-xmark"></i>
+    `;
+     
+
+    listItem.addEventListener(
+            "click" , function(){
+            this.classList.toggle ("done");
+        }
+        )
+
+        listItem.querySelector("i").addEventListener("click", function(){
+            listItem.remove()
+    }  
+      )
+        
 
     toDOBox.appendChild(listItem);
+
+    // console.log(listItem);
 }
